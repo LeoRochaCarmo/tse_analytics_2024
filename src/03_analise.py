8ce404adcb5cba1536896015adf3b9143d1ffce4
leoRochaCarmo <leorochaengenharia@gmail.com>
2025-04-03 20:24:36 -0300

    finalizando gráfico - Partidos: Cor vs Gênero - Eleições 2024

--- src/03_analise.py
+++ src/03_analise.py
@@ -4,6 +4,8 @@ import pandas as pd
 import sqlalchemy
 import seaborn as sn
 import matplotlib.pyplot as plt
+import matplotlib.image as img
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 with open ('partidos.sql', 'r') as open_file:
      query = open_file.read()
@@ -11,8 +13,6 @@ with open ('partidos.sql', 'r') as open_file:
 engine = sqlalchemy.create_engine("sqlite:///..\\data\\database.db")
 
 df = pd.read_sql_query(query, engine)
-
-df.head()
 
 #%%
 
@@ -24,7 +24,7 @@ txCorRacaPretaParda = df ['totalCorRacaPretaParda'].sum() / df['totalCandidatura
 #%%
 
 from adjustText import adjust_text 
-plt.figure(dpi=500)
+plt.figure(dpi=400)
 
 sn.scatterplot(data=df, 
                x='txGenFemininoBR', 
@@ -37,20 +37,40 @@ for i in df['SG_PARTIDO']:
      y = data['txCorRacaPretaBR'].values[0]
      texts.append(plt.text(x, y, i, fontsize=9))
 
-
 adjust_text(texts, 
             only_move={'points': 'y', 'texts': 'xy'}, 
             arrowprops=dict(arrowstyle='->', color='gray'))
-
 
 plt.grid(True)
 plt.title('Partidos: Cor vs Gênero - Eleições 2024')
 plt.xlabel('Taxa de Mulheres')
 plt.ylabel('Taxa de Pessoas Pretas')
-plt.hlines(y=txCorRacaPreta, xmin=0.3, xmax=0.55, color='black', linestyles='--', label='Taxa Pretos')
-plt.vlines(x=txGenFeminino, ymin=0.05, ymax=0.40, color='tomato', linestyles='--', label='Taxa Mulheres')
+plt.hlines(y=txCorRacaPreta, 
+           xmin=0.3, 
+           xmax=0.55, 
+           color='black', 
+           linestyles='--', 
+           label=f'Pretos Geral: {100 * txCorRacaPreta:.2f}%')
+
+plt.vlines(x=txGenFeminino, 
+           ymin=0.05, 
+           ymax=0.40, 
+           color='tomato', 
+           linestyles='--', 
+           label=f'Mulheres Geral: {100 * txGenFeminino:.2f}%')
 
 plt.legend(loc='lower right', title='Partidos', title_fontsize=10)
+
+logo = img.imread('..\\img\\copyright.png')
+imagebox = OffsetImage(logo, zoom=0.065, alpha=0.45)
+ab = AnnotationBbox(imagebox, (0.76, 0.44), 
+                    frameon=False, 
+                    pad=0, 
+                    xycoords='axes fraction', 
+                    boxcoords='axes fraction', 
+                    box_alignment=(0.5, 0.5))
+
+plt.gca().add_artist(ab)
 
 plt.savefig('..\\img\\partidos_cor_raca_genero.png')
 
